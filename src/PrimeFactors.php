109878fdbbabcba76aa5378a0895cbd784d5b9b7
sora_uzu <naruto07170717@gmail.com>
2023-10-31 21:09:20 +0900

    prime factors kata

---
+++ src/PrimeFactors.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+class PrimeFactors
+{
+  public function generate($number)
+  {
+    $factors = [];
+    $divisor = 2;
+
+    while ($number > 1) {
+      while ($number % $divisor === 0) {
+        $factors[] = $divisor;
+        $number = $number / $divisor;
+      }
+      $divisor++;
+    }
+    return $factors;
+  }
+}
